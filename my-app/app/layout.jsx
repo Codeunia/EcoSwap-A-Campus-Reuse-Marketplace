095998444c6f96857bc13eb21936f33306b9f336
Sneha <snehaarora68791@gmail.com>
2025-08-18 11:58:47 +0530

    complete projecct working

--- my-app/app/layout.jsx
+++ my-app/app/layout.jsx
@@ -1,21 +1,3 @@
-// import './globals.css'
-
-// export const metadata = {
-//   title: 'EcoSwap',
-//   description: 'Campus Reuse Marketplace'
-// }
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en">
-//       <body className="flex flex-col min-h-screen">
-//         <main className="flex-grow">{children}</main>
-//       </body>
-//     </html>
-//   )
-// }
-
-// app/layout.js
 import { AuthProvider } from "../context/AuthContext";
 import "./globals.css";
 
